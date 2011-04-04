b21967b413fe50d7668fdf0393a14f0e3d346466
Cristian Hampus <contact@cristianhampus.se>
2011-04-04 08:00:43 +0200

    Made the Bun working.

--- bun.php
+++ bun.php
@@ -8,24 +8,98 @@
  * @package Bun
  */
 
-if (file_exists('.htaccess')) {
-    throw new Exception('Bun needs an .htaccess file to work properly.');
+/**
+ *  Check if the .htaccess file exists. 
+ */
+if (!file_exists('.htaccess')) {
+    //throw new Exception('Bun needs an .htaccess file to work properly.');
 }
 
 class Bun {
-    public function route($method, $path, $callback)
-    {
+    /**
+     * True of route has been matched.  
+     * 
+     * @var bool
+     */
+    private $route_matched = FALSE;
+    
+    /**
+     * Static variable containing the bun.
+     *
+     * @var Bun
+     */
+    private static $instance = NULL;
+
+    private function __construct() {
         
     }
 
-    public function router()
+    /**
+     * Get an instance of the bun. 
+     * 
+     * @return Bun
+     */
+    private static function instance() {
+        if (is_null(self::$instance)) {
+            self::$instance = new Bun();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Checks if the route matches. 
+     * 
+     * @param string $method 
+     * @param string $path 
+     * @param callback $callback 
+     * @return void
+     */
+    public static function route($method, $path, $callback)
     {
-        
+        $bun = Bun::instance();
+
+        if ($bun->route_matched) {
+            return FALSE;
+        }
+
+        $requested_path = (isset($_SERVER['PATH_INFO'])) ? $_SERVER['PATH_INFO'] : '/';
+
+        if ($method === $_SERVER['REQUEST_METHOD']) {
+            if ($path === $requested_path) {
+                call_user_func($callback);
+
+                return $bun->route_matched = TRUE;
+            }
+        }
+    }
+
+    public function getCurrentUrl()
+    {
+        $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
+        $url .= sprintf('://%s%s', $_SERVER['HTTP_HOST'], $_SERVER['SCRIPT_NAME']);
+
+        $url .= (isset($_SERVER['PATH_INFO'])) ? $_SERVER['PATH_INFO'] : '';
+        return $this->arrayToObject(parse_url($url));
+    }
+
+    /**
+     * Turns an array to an object and returns it. 
+     * 
+     * @param Array $array 
+     * @return stdClass
+     */
+    private function arrayToObject(Array $array)
+    {
+        $object = new stdClass;
+
+        foreach ($array as $key => $value) {
+            $object->$key = $value;
+        }
+
+        return $object;
     }
 }
-
-$bun = new Bun();
-
 
 /**
  *  The public API for the framework. 
@@ -40,7 +114,7 @@ $bun = new Bun();
  * @return void
  */
 function get($path, $callback) {
-    $bun->route('GET', $path, $callback);
+    Bun::route('GET', $path, $callback);
 }
 
 /**
@@ -52,7 +126,7 @@ function get($path, $callback) {
  * @return void
  */
 function post($path, $callback) {
-    $bun->route('POST', $path, $callback);
+    Bun::route('POST', $path, $callback);
 }
 
 /**
@@ -64,7 +138,7 @@ function post($path, $callback) {
  * @return void
  */
 function put($path, $callback) {
-    $bun->route('PUT', $path, $callback);
+    Bun::route('PUT', $path, $callback);
 }
 
 /**
@@ -76,5 +150,5 @@ function put($path, $callback) {
  * @return void
  */
 function delete($path, $callback) {
-    $bun->route('DELETE', $path, $callback);
+    Bun::route('DELETE', $path, $callback);
 }
